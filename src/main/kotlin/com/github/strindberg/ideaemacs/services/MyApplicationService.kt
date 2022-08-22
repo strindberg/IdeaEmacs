19473e3eaf8d1f5c4ac1be9b3b7c33afc8b61b1d
GitHub Action <action@github.com>
2022-08-22 06:13:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/strindberg/ideaemacs/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/strindberg/ideaemacs/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.strindberg.ideaemacs.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.strindberg.ideaemacs.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/strindberg/ideaemacs/services/MyProjectService.kt
+++ src/main/kotlin/com/github/strindberg/ideaemacs/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.strindberg.ideaemacs.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.strindberg.ideaemacs.MyBundle
 
 class MyProjectService(project: Project) {
 
